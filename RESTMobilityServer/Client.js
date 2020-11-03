dcaf59ae1719d436801f506874809aa58556ad5c
woodencatty <woodencatty@gmail.com>
2020-11-03 21:21:12 +0900

    time

--- RESTMobilityServer/Client.js
+++ RESTMobilityServer/Client.js
@@ -63,7 +63,7 @@ setInterval(() => {
       });
 }, 10);
 */
-
+console.time('RTT');
 request(request_option1, function (error, response, body) {
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
@@ -96,6 +96,7 @@ request(request_option1, function (error, response, body) {
                                 console.error('error:', error); // Print the error if one occurred
                                 console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
                                 console.log('body:', body); // Print the HTML for the Google homepage.
+                                console.timeEnd('RTT');
                               });
                           });
                       });
