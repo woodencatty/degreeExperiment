11ad31491f2274dbdccff38c7d1705138c4c00ac
woodencatty <woodencatty@gmail.com>
2020-11-03 21:25:31 +0900

    sav

--- RESTMobilityServer/Client.js
+++ RESTMobilityServer/Client.js
@@ -51,52 +51,20 @@ const request_option8 = {
         'dummies' : 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante i'
     }
 };
-/*
+
 var i = 0;
 setInterval(() => {
-    request(request_option, function (error, response, body) {
-        console.error('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-        console.log(i);
-        i++;
-      });
-}, 10);
-*/
-console.time('RTT');
-request(request_option1, function (error, response, body) {
-    console.error('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-    request(request_option2, function (error, response, body) {
-        console.error('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-        request(request_option3, function (error, response, body) {
-            console.error('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
-            request(request_option4, function (error, response, body) {
-                console.error('error:', error); // Print the error if one occurred
-                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-                console.log('body:', body); // Print the HTML for the Google homepage.
-                request(request_option5, function (error, response, body) {
-                    console.error('error:', error); // Print the error if one occurred
-                    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-                    console.log('body:', body); // Print the HTML for the Google homepage.
-                    request(request_option6, function (error, response, body) {
-                        console.error('error:', error); // Print the error if one occurred
-                        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-                        console.log('body:', body); // Print the HTML for the Google homepage.
-                        request(request_option7, function (error, response, body) {
-                            console.error('error:', error); // Print the error if one occurred
-                            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-                            console.log('body:', body); // Print the HTML for the Google homepage.
-                            request(request_option8, function (error, response, body) {
-                                console.error('error:', error); // Print the error if one occurred
-                                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-                                console.log('body:', body); // Print the HTML for the Google homepage.
-                                console.timeEnd('RTT');
+    console.time('RTT');
+    request(request_option1, function (error, response, body) {
+        request(request_option2, function (error, response, body) {
+            request(request_option3, function (error, response, body) {
+                request(request_option4, function (error, response, body) {
+                    request(request_option5, function (error, response, body) {
+                        request(request_option6, function (error, response, body) {
+                            request(request_option7, function (error, response, body) {
+                                request(request_option8, function (error, response, body) {
+                                    console.timeEnd('RTT');
+                                  });
                               });
                           });
                       });
@@ -104,4 +72,6 @@ request(request_option1, function (error, response, body) {
               });
           });
       });
-  });
+        console.log(i);
+        i++;
+}, 100);
