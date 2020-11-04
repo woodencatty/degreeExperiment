6dac5009094077bafc943c3bc7d2593c6f70fc68
woodencatty <woodencatty@gmail.com>
2020-11-04 17:47:23 +0900

    percentage

--- RESTMobilityServer/server/routes/index.js
+++ RESTMobilityServer/server/routes/index.js
@@ -3,6 +3,30 @@ var router = express.Router();
 
 const request = require('request');
 
+var pidusage = require('pidusage')
+ 
+
+
+setInterval(() => {
+    pidusage(process.pid, function (err, stats) {
+        console.log("CPU : "+stats.cpu + "%")
+        console.log("MEM : "+stats.memory+"bytes")
+        console.log();
+        
+        // => {
+        //   cpu: 10.0,            // percentage (from 0 to 100*vcore)
+        //   memory: 357306368,    // bytes
+        //   ppid: 312,            // PPID
+        //   pid: 727,             // PID
+        //   ctime: 867000,        // ms user + system time
+        //   elapsed: 6650000,     // ms since the start of the process
+        //   timestamp: 864000000  // ms since epoch
+        // }
+        cb()
+      })
+    
+}, 1000);
+
 var mobility_request1 = {
   url: 'http://59.9.86.21:3001/request',
   headers: {
