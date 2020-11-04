1bffa08cc3d0d452a6a61e512aa89a2ef8da6127
woodencatty <woodencatty@gmail.com>
2020-11-04 15:23:55 +0900

    tst

--- RESTMobilityServer/Client2.js
+++ RESTMobilityServer/Client2.js
@@ -1,24 +1,24 @@
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
-const { exit } = require('process');
-const request = require('request');
-
-const user_request = {
-    url: 'http://59.9.86.21:3000/request',
+var http = require('http');
+var options = {
+    hostname: '127.0.0.1',
+    port: '3000',
+    path: '/request',
+  method: 'GET',
     headers: {
         'dummies': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante i'
     }
-};
-var i = 0;
-setInterval(() => {
-requestsent();
-
-}, 100);
-
-
-async function requestsent () {
-    var i = Math.floor(Math.random() * 100000) + 1
-    console.time(i);
-    request(user_request, function (error, response, body) {
-        console.timeEnd(i);
+  };
+ 
+  var req = http.request(options, function(res) {
+    console.log('Status: ' + res.statusCode);
+    console.log('Headers: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (body) {
+      console.log('Body: ' + body);
     });
-  }
+  });
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+   
+  req.end();
