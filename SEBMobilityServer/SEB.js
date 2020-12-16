eb096e4feb39b8cada6c5708d6725a58a54b8996
woodencatty <woodencatty@gmail.com>
2020-12-16 18:28:13 +0900

    seb to mosca

--- SEBMobilityServer/SEB.js
+++ SEBMobilityServer/SEB.js
@@ -1,16 +1,37 @@
-var aedes = require('aedes')()
-var server = require('net').createServer(aedes.handle)
-const request = require('request');
-var port = 1883
-
-server.listen(port, function() {
-  console.log('server listening on port', port);
+var mosca = require('mosca');
+ 
+var ascoltatore = {
+  //using ascoltatore
+  type: 'mongo',
+  url: 'mongodb://localhost:27017/mqtt',
+  pubsubCollection: 'ascoltatori',
+  mongo: {}
+};
+ 
+var settings = {
+  port: 1883,
+  backend: ascoltatore
+};
+ 
+var server = new mosca.Server(settings);
+ 
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);
 });
+ 
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published', packet.payload);
+});
+ 
+server.on('ready', setup);
+ 
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running');
+}
 
-setInterval(() => {
-  console.log("alive"  )
-}, 500);
-
+/*
 aedes.subscribe('/mobility002', function(packet, cb) {
   var user_request = {
     url: 'http://210.102.181.219:8080/request',
@@ -20,37 +41,4 @@ aedes.subscribe('/mobility002', function(packet, cb) {
   };
   request(user_request, function (error, response, body) { console.log("gone");
   })
-});
-
-aedes.subscribe('/user002', function(packet, cb) {
-  var user_request = {
-    url: 'http://210.102.181.219:8080/request',
-    headers: {
-        'packet':  packet.payload.toString()
-    }
-  };
-  request(user_request, function (error, response, body) { console.log("gone");
-  })
-});
-
-aedes.subscribe('/mobility001', function(packet, cb) {
-  var user_request = {
-    url: 'http://210.102.181.219:8080/request',
-    headers: {
-        'packet':  packet.payload.toString()
-    }
-  };
-  request(user_request, function (error, response, body) { console.log("gone");
-  })
-});
-
-aedes.subscribe('/user001', function(packet, cb) {
-  var user_request = {
-    url: 'http://210.102.181.219:8080/request',
-    headers: {
-        'packet':  packet.payload.toString()
-    }
-  };
-  request(user_request, function (error, response, body) { console.log("gone");
-  })
-});
+});*/
