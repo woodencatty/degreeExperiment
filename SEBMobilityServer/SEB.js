b6866e29b8e20cb9a689e93fc8dc32adbef6b830
woodencatty <woodencatty@gmail.com>
2020-12-16 18:06:57 +0900

    fix

--- SEBMobilityServer/SEB.js
+++ SEBMobilityServer/SEB.js
@@ -6,18 +6,47 @@ server.listen(port, function() {
   console.log('server listening on port', port);
 });
 
+
 aedes.subscribe('/mobility002', function(packet, cb) {
-  console.log('Published', packet.payload.toString());
+  var user_request = {
+    url: 'http://210.102.181.219:8080/request',
+    headers: {
+        'packet':  packet.payload.toString()
+    }
+  };
+  request(user_request, function (error, response, body) {
+  })
 });
 
 aedes.subscribe('/user002', function(packet, cb) {
-  console.log('Published', packet.payload.toString());
+  var user_request = {
+    url: 'http://210.102.181.219:8080/request',
+    headers: {
+        'packet':  packet.payload.toString()
+    }
+  };
+  request(user_request, function (error, response, body) {
+  })
 });
 
 aedes.subscribe('/mobility001', function(packet, cb) {
-  console.log('Published', packet.payload.toString());
+  var user_request = {
+    url: 'http://210.102.181.219:8080/request',
+    headers: {
+        'packet':  packet.payload.toString()
+    }
+  };
+  request(user_request, function (error, response, body) {
+  })
 });
 
 aedes.subscribe('/user001', function(packet, cb) {
-  console.log('Published', packet.payload.toString());
+  var user_request = {
+    url: 'http://210.102.181.219:8080/request',
+    headers: {
+        'packet':  packet.payload.toString()
+    }
+  };
+  request(user_request, function (error, response, body) {
+  })
 });
