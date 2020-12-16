87d09d9e5c85afce63e74ee77e40540a4041c4d1
woodencatty <woodencatty@gmail.com>
2020-12-16 13:39:23 +0900

    work?

--- RESTMobilityServer/mother_of_Client.js
+++ RESTMobilityServer/mother_of_Client.js
@@ -1,4 +1,5 @@
 const { exec } = require('child_process');
+const fs = require('fs');
 
 var count = 0;
 
@@ -6,6 +7,14 @@ var setThread = setInterval(() => {
     count++;
 
     exec('node Client.js', (error, stdout, stderr) => {
+
+
+      var access = fs.createWriteStream('/sdcard/Download/client'+count+'_stats.log');
+      stdout = access.write.bind(access);
+      process.on('uncaughtException', function(err) {
+          //console.error((err && err.stack) ? err.stack : err);
+        });
+
         if (error) {
           console.error(`exec error: ${error}`);
           return;
